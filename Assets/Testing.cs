6b912d925f7a7fca0b76d63d6fd7ac86e875a143
Duc Anh <88607235+BinBlun@users.noreply.github.com>
2024-08-31 20:29:33 +0700

    Update

---
+++ Assets/Testing.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace DOTS_Course
+{
+    public class Testing: MonoBehaviour
+    {
+        [SerializeField] private int _abc;
+
+        private void Update()
+        {
+            
+        }
+    }
+}
