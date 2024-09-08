fe98861ecf8863214e0957589ce95c10eb220ca8
Duc Anh <binflinterfler292001@gmail.com>
2024-09-08 15:00:39 +0700

    Generate move position

--- Assets/Scripts/MonoBehaviour/UnitSelectionManager.cs
+++ Assets/Scripts/MonoBehaviour/UnitSelectionManager.cs
@@ -1,11 +1,10 @@
 using System;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Mathematics;
 using Unity.Physics;
 using Unity.Transforms;
 using UnityEngine;
-using Ray = Unity.Physics.Ray;
-using RaycastHit = Unity.Physics.RaycastHit;
 
 
 public class UnitSelectionManager : MonoBehaviour
@@ -97,7 +96,6 @@ public class UnitSelectionManager : MonoBehaviour
                     {
                         entityManager.SetComponentEnabled<Selected>(raycastHit.Entity, true);
                     }
-                    
                 }
             }
 
@@ -118,12 +116,13 @@ public class UnitSelectionManager : MonoBehaviour
             NativeArray<Entity> entityArray = entityQuery.ToEntityArray(Allocator.Temp);
             //Query het UnitMover cho vao native array (local array)
             NativeArray<UnitMover> unitMoverArray = entityQuery.ToComponentDataArray<UnitMover>(Allocator.Temp);
+            NativeArray<float3> movePositionArray = GenerateMovePositionArray(mouseWorldPosition, entityArray.Length);
             for (int i = 0; i < unitMoverArray.Length; i++)
             {
                 //Tao copy 
                 UnitMover unitMover = unitMoverArray[i];
                 //Modify copy
-                unitMover.TargetPosition = mouseWorldPosition;
+                unitMover.TargetPosition = movePositionArray[i];
 
                 //Cach 1:
                 //Save data copy vao data goc
@@ -152,5 +151,48 @@ public class UnitSelectionManager : MonoBehaviour
             upperRightCorner.x - lowerLeftCorner.x,
             upperRightCorner.y - lowerLeftCorner.y
         );
+    }
+
+    private NativeArray<float3> GenerateMovePositionArray(float3 targetPosition, int positionCount)
+    {
+        NativeArray<float3> positionArray = new NativeArray<float3>(positionCount, Allocator.Temp);
+        if (positionCount == 0)
+        {
+            return positionArray;
+        }
+
+        positionArray[0] = targetPosition;
+        if (positionCount == 1)
+        {
+            return positionArray;
+        }
+
+        float ringSize = 2.2f;
+        int ring = 0;
+        int positionIndex = 1;
+
+        while (positionIndex < positionCount)
+        {
+            int ringPositionCount = 3 + ring * 2;
+
+            for (int i = 0; i < ringPositionCount; i++)
+            {
+                float angle = i * (math.PI2 / ringPositionCount);
+                float3 ringVector = math.rotate(quaternion.RotateY(angle), new float3(ringSize * (ring + 1), 0, 0));
+                float3 ringPosition = targetPosition + ringVector;
+
+                positionArray[positionIndex] = ringPosition;
+                positionIndex++;
+
+                if (positionIndex >= positionCount)
+                {
+                    break;
+                }
+            }
+
+            ring++;
+        }
+
+        return positionArray;
     }
 }
